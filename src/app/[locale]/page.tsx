7e6a3c374e1e68778bc86d9661585a6252cf1c7d
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2025-03-24 13:53:48 +0100

    remove cv

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -80,7 +80,6 @@ export default function HomePage() {
   return (
   <div className="w-full h-max">
     <audio id="clickSound" src="/sound.mp3"></audio>
-
    <HeroHighlight className="w-full px-5 sm:px-8">
   <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between text-center lg:text-left space-y-8 lg:space-y-0 lg:space-x-8 mt-12 w-full h-max">
     <div className="flex flex-col items-center lg:items-start max-w-3xl w-full">
@@ -89,20 +88,20 @@ export default function HomePage() {
       <h2 className="font-semibold text-green-500 mb-4">
         <TypewriterEffectSmooth words={words}/>
       </h2>
-      <div className="flex space-x-4 pb-4">
+      <div className="w-full pb-4 px-8 lg:px-0">
     <button
-    className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-sm"
+    className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-sm w-full max-w-md "
     onClick={playSoundOnClick}
     >
-      <Anchor href="https://wa.link/t148zf" title={t('firstButon')}/>
+      <Anchor href="https://wa.link/a2ssmy" title={t('firstButon')}/>
     </button>
 
-    <button 
+    {/* <button 
     className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-white dark:border-green-800 dark:text-slate-200 text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400 text-sm"
     onClick={playSoundOnClick}
     >
       <Anchor href="https://teal-rozamond-56.tiiny.site/" title={t('secondButton')}/>
-    </button>
+    </button> */}
   </div>
 </div>
     <HoverBorderGradient
