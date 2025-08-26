e8ce3ed1629dfc7fd6d31913d9251737581bc70d
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2025-08-26 21:48:36 +0100

    api route link

--- src/app/[locale]/components/ContactForm.tsx
+++ src/app/[locale]/components/ContactForm.tsx
@@ -24,7 +24,7 @@ export function ContactForm() {
 
 
   try {
-    const response = await fetch('../../api/send', {
+    const response = await fetch('/api/send', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, message }),
